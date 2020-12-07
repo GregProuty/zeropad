1c429ac09674cadb24111c64bca4fe909849798a
gregoryprouty <gprout009@gmail.com>
2020-12-07 13:37:29 -0800

    cleanup

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 // Given a string, zero pad any whole number to X char.
 
 // Examples:
-
 // "area 59", 4
-
 // return "area 0059"
-
 // "123 Foo st", 5
-
 // return "00123 Foo st"
-
 // "123 Foo st", 2
-
 // return "123 Foo st"
-
 // "Area59asdf234", 4
-
 // return "Area0059asdf0234"
 
-// const parseNumbers = str => {
-
-// }
 const pad = (int, digits) =>
   int.length >= digits
     ? int
